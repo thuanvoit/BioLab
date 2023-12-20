ef474ce6cd3e504ea5b0385047ba1bf1f2e01641
Thuan Vo <thuanvo09@hotmail.com>
2023-12-20 16:25:18 -0500

    fix tracked files

--- frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import liveReload from "vite-plugin-live-reload";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), liveReload("./src/**/*")],
-});
